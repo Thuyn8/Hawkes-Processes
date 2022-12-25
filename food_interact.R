a0a3a4bf210276206d582c318997c455cc74637d
Thuy Linh Nguyen <92179640+Thuyn8@users.noreply.github.com>
2022-12-25 14:21:58 -0800

    chang
    New dfsdsfdfsdf

--- food_interact.R
+++ food_interact.R
@@ -2,10 +2,12 @@ library(stelfi)
 library(dplyr)
 library(gridExtra)
 library(ggplot2)
+
+setwd(path.expand("~/Dropbox/SharedRScript"))
 # Loading  the two feeding datasets
 S2 <- read.csv("~/Documents/Hawkes Processes/report/feeding/S2Data.csv") %>% 
   select(-c("Date", "Time", "MSec", "Sample")) 
- 
+
 
 S20 <- read.csv("~/Documents/Hawkes Processes/report/feeding/S20Data.csv") %>% 
   select(-c("Date", "Time", "MSec", "Sample")) 
@@ -22,19 +24,29 @@ S2_thres <- as.data.frame(sapply(S2[, !(colnames(S2) %in% c("D6W5", "D19W10"))],
 S20_thres <- as.data.frame(sapply(S20, function(i){as.integer(i>=thres)})) %>%
   mutate(Time = 1: nrow(S20))
 
-# creating L2, L20 (time points for each fly)
+# creating L2 and L20 data sets including spike times for each fly
+## sugar 2% (s2)
 L2 <- lapply(S2_thres[,-ncol(S2_thres)], function(i){S2_thres$Time[i == 1]})
+## sugar 20% (s20)
 L20 <-lapply(S20_thres[,-ncol(S20_thres)], function(i){S20_thres$Time[i ==1]})
 
+## Initial parameters
 params <- c(mu = 0.05, alpha = 0.05, beta = 1)
 
 
-## estimate mu_0, alpha, beta
-est_S2 <-as.data.frame(t(do.call(cbind, lapply(L2, function(i){get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]}))))
+# estimate mu_0, alpha, beta using LH 
+## s2
+est_S2 <-as.data.frame(t(do.call(cbind, 
+                                 lapply(L2, function(i){
+                                   get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]
+                                 }))))
+## s20
+est_S20 <-as.data.frame(t(do.call(cbind,
+                                  lapply(L20, function(i){
+                                    get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]
+                                  }))))
 
-est_S20 <-as.data.frame(t(do.call(cbind, lapply(L20, function(i){get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]}))))
-
-## obtain intensity rates
+## obtain intensity rates for each data set
 
 rate2 <- lapply(L2, function(i){show_hawkes_GOF(fit_hawkes(times = i,parameters = params), 
                                                 plot = F,
@@ -48,4 +60,4 @@ rate2 <-lapply(L20, function(i){show_hawkes_GOF(fit_hawkes(times = i, parameters
 #show_hawkes_GOF(fit_hawkes(times = L2[[i]], parameters = params))
 
 
-      
+
