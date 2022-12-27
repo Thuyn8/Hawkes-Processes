cef4a2fbbaddbf270b2a3ea5b779e9a0994e9488
Thuy Linh Nguyen <92179640+Thuyn8@users.noreply.github.com>
2022-12-26 18:53:55 -0800

    new

--- food_interact.R
+++ food_interact.R
@@ -2,8 +2,8 @@ library(stelfi)
 library(dplyr)
 library(gridExtra)
 library(ggplot2)
+library(VIM)
 
-setwd(path.expand("~/Dropbox/SharedRScript"))
 # Loading  the two feeding datasets
 S2 <- read.csv("~/Documents/Hawkes Processes/report/feeding/S2Data.csv") %>% 
   select(-c("Date", "Time", "MSec", "Sample")) 
@@ -18,11 +18,13 @@ sapply(S2, max)
 
 # Creating data with threshold >=30
 thres <- 30
-S2_thres <- as.data.frame(sapply(S2[, !(colnames(S2) %in% c("D6W5", "D19W10"))], 
-                                 function(i){as.integer(i>=thres)}))%>%
-  mutate(Time = 1: nrow(S2))
-S20_thres <- as.data.frame(sapply(S20, function(i){as.integer(i>=thres)})) %>%
-  mutate(Time = 1: nrow(S20))
+S2_thres <- sapply(S2[, !(colnames(S2) %in% c("D6W5", "D19W10"))], 
+                                 function(i){as.integer(i>=thres)})%>%
+            as.data.frame() %>% 
+            mutate(Time = 1: nrow(S2)) # create Time column
+S20_thres <- sapply(S20, function(i){as.integer(i>=thres)}) %>%
+            as.data.frame() %>%
+            mutate(Time = 1: nrow(S20)) # Create Time column
 
 # creating L2 and L20 data sets including spike times for each fly
 ## sugar 2% (s2)
@@ -36,15 +38,15 @@ params <- c(mu = 0.05, alpha = 0.05, beta = 1)
 
 # estimate mu_0, alpha, beta using LH 
 ## s2
-est_S2 <-as.data.frame(t(do.call(cbind, 
-                                 lapply(L2, function(i){
-                                   get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]
-                                 }))))
+est_S2 <-t(do.call(cbind, 
+                   lapply(L2, function(i){
+                     get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]}))) %>% 
+          as.data.frame()
 ## s20
-est_S20 <-as.data.frame(t(do.call(cbind,
-                                  lapply(L20, function(i){
-                                    get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]
-                                  }))))
+est_S20 <-t(do.call(cbind,
+                    lapply(L20, function(i){
+                      get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]}))) %>%
+          as.data.frame()
 
 ## obtain intensity rates for each data set
 
@@ -60,4 +62,47 @@ rate2 <-lapply(L20, function(i){show_hawkes_GOF(fit_hawkes(times = i, parameters
 #show_hawkes_GOF(fit_hawkes(times = L2[[i]], parameters = params))
 
 
+#  Read the Social interaction data sets
+## Control male data
+control <- read.csv("~/Documents/Hawkes Processes/report/social_interact/ControlMale.csv") 
+dim(control)
+## NPF chrimson Male data
+npf <- read.csv("~/Documents/Hawkes Processes/report/social_interact/NPFChrimsonMale.csv") 
+dim(npf)
 
+## convert data to 0 and 1 using  given threshold 8mm
+thres_dist <- 8
+
+control <- sapply(control, function(i){as.integer(i>=thres_dist)}) %>% 
+            as.data.frame() %>% 
+            mutate(Time = 1:nrow(control))
+head(control)
+
+npf <- sapply(npf, function(i){as.integer(i>=thres_dist)}) %>% 
+        as.data.frame() %>% 
+        mutate(Time = 1:nrow(npf))
+head(npf)
+
+## Check missing data
+sapply(control, function(i){100*mean(is.na(i))}) %>%signif(3)  # T2L,T4L have NAs
+sapply(npf, function(i){100*mean(is.na(i))})%>% signif(3)  # T3R has missing
+## missing pattern plots --> monotone missing pattern
+aggr(control, numbers = T, gap = 2,
+     cex.axis = .5,
+     labels = colnames(control),
+     sortVars = T,
+     ylab = c("Histogram of NAs-Control data", "Pattern of missing data"), only.miss = T)
+mtext("Figure 4: Pattern of missing data of Control Male", side =1, line = 7, outer= T )
+
+aggr(npf, numbers = T, gap = 2,
+     cex.axis = .5,
+     labels = colnames(npf),
+     sortVars = T,
+     ylab = c("Histogram of NAs-npf data", "Pattern of missing data"), only.miss = T)
+mtext("Figure 4: Pattern of missing data of Control Male", side =1, line = 7, outer= T )
+
+## check if any flies have no social interaction
+sapply(control, max, na.rm = T) # All flies interacted at least  once 
+sapply(npf, max, na.rm = T)     # All flies interacted at least  once 
+
+## create 
