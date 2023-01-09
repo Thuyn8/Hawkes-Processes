ed1862c3b5cc0bc76b777dfd393223735adf5f8e
Thuy Linh Nguyen <92179640+Thuyn8@users.noreply.github.com>
2023-01-08 21:50:28 -0800

    update
    update

--- food_interact.R
+++ food_interact.R
@@ -3,8 +3,9 @@ library(dplyr)
 library(gridExtra)
 library(ggplot2)
 library(VIM)
+library(hawkesbow)
 
-# Loading  the two feeding datasets
+# 1. Loading  the two feeding datasets
 S2 <- read.csv("~/Documents/Hawkes Processes/report/feeding/S2Data.csv") %>% 
   select(-c("Date", "Time", "MSec", "Sample")) 
 
@@ -12,13 +13,13 @@ S2 <- read.csv("~/Documents/Hawkes Processes/report/feeding/S2Data.csv") %>%
 S20 <- read.csv("~/Documents/Hawkes Processes/report/feeding/S20Data.csv") %>% 
   select(-c("Date", "Time", "MSec", "Sample")) 
 
-# Two flies didn't interact with S2 food : D6W5, D19W10. 
-## All flies interact at least once with S20
+## a.  Two flies didn't interact with S2 food : D6W5, D19W10. 
+## b. All flies interact at least once with S20
 sapply(S2, max)
 
-# Creating data with threshold >=30
+## c. Creating data with threshold >=30
 thres <- 30
-S2_thres <- sapply(S2[, !(colnames(S2) %in% c("D6W5", "D19W10"))], 
+S2_thres <- sapply(S2[, !(colnames(S2) %in% c("D6W5", "D19W10"))],  # rm two flies D6W5, D19W10
                                  function(i){as.integer(i>=thres)})%>%
             as.data.frame() %>% 
             mutate(Time = 1: nrow(S2)) # create Time column
@@ -26,29 +27,30 @@ S20_thres <- sapply(S20, function(i){as.integer(i>=thres)}) %>%
             as.data.frame() %>%
             mutate(Time = 1: nrow(S20)) # Create Time column
 
-# creating L2 and L20 data sets including spike times for each fly
-## sugar 2% (s2)
+## d. creating L2 and L20 data sets including spike times for each fly
+### sugar 2% (s2)
 L2 <- lapply(S2_thres[,-ncol(S2_thres)], function(i){S2_thres$Time[i == 1]})
-## sugar 20% (s20)
+### sugar 20% (s20)
 L20 <-lapply(S20_thres[,-ncol(S20_thres)], function(i){S20_thres$Time[i ==1]})
 
-## Initial parameters
+# 2. Independent Hawkes processes
+## a. Initial parameters
 params <- c(mu = 0.05, alpha = 0.05, beta = 1)
 
 
-# estimate mu_0, alpha, beta using LH 
-## s2
+## b. estimate mu_0, alpha, beta using LH 
+
 est_S2 <-t(do.call(cbind, 
                    lapply(L2, function(i){
                      get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]}))) %>% 
           as.data.frame()
-## s20
+
 est_S20 <-t(do.call(cbind,
                     lapply(L20, function(i){
                       get_coefs(fit_hawkes(times = i, parameters = params))[1:3,1]}))) %>%
           as.data.frame()
 
-## obtain intensity rates for each data set
+## c. obtain intensity rates for each fly in each data set
 
 rate2 <- lapply(L2, function(i){show_hawkes_GOF(fit_hawkes(times = i,parameters = params), 
                                                 plot = F,
@@ -57,9 +59,12 @@ rate2 <-lapply(L20, function(i){show_hawkes_GOF(fit_hawkes(times = i, parameters
                                                 plot = F,
                                                 return_values = T)$interarrivals})
 
-## plots
+## d. plots 
+show_hawkes_GOF(fit_hawkes(times = L2[[1]], parameters = params))
 
-#show_hawkes_GOF(fit_hawkes(times = L2[[i]], parameters = params))
+# 3. Mutually exciting Hawkes process
+## a. 
+
 
 
 #  Read the Social interaction data sets
@@ -73,20 +78,23 @@ dim(npf)
 ## convert data to 0 and 1 using  given threshold 8mm
 thres_dist <- 8
 
-control <- sapply(control, function(i){as.integer(i>=thres_dist)}) %>% 
+control_h <- sapply(control, function(i){as.integer(i>=thres_dist)}) %>% 
             as.data.frame() %>% 
             mutate(Time = 1:nrow(control))
-head(control)
+head(control_h)
 
-npf <- sapply(npf, function(i){as.integer(i>=thres_dist)}) %>% 
+npf_h <- sapply(npf, function(i){as.integer(i>=thres_dist)}) %>% 
         as.data.frame() %>% 
         mutate(Time = 1:nrow(npf))
-head(npf)
+head(npf_h)
 
+## create hawkes data
+control_hwk <- lapply(control_h[,-ncol(control_h)], function(i){control_h$Time[i ==1]})
 ## Check missing data
-sapply(control, function(i){100*mean(is.na(i))}) %>%signif(3)  # T2L,T4L have NAs
-sapply(npf, function(i){100*mean(is.na(i))})%>% signif(3)  # T3R has missing
+sapply(control_hwk, function(i){100*mean(is.na(i))}) %>%signif(3)  # T2L,T4L have NAs
+sapply(npf_h, function(i){100*mean(is.na(i))})%>% signif(3)  # T3R has missing
 ## missing pattern plots --> monotone missing pattern
+
 aggr(control, numbers = T, gap = 2,
      cex.axis = .5,
      labels = colnames(control),
